f4c17a56b78da3b0903d394ac5d937e2d51c8f47
Grace Wambui <wambuigm51@yahoo.com>
2025-10-01 18:48:42 +0300

    Add Tailwind CSS integration and update BusinessCard component styles
    - Added Tailwind CSS as a dependency in package.json and pnpm-lock.yaml.
    - Updated App.css to import Tailwind CSS.
    - Refactored BusinessCard component to use Tailwind CSS for styling.
    - Removed unused index.css file.
    - Updated main.jsx to remove import of index.css.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- Day-01/src/components/BusinessCard.jsx
+++ Day-01/src/components/BusinessCard.jsx
@@ -3,15 +3,15 @@ import myImage from '../assets/me.jpeg'
 
 const BusinessCard = () => {
   return (
-    <div className='card'>
-        <img src={myImage} alt="Picture of Grace" className='card-photo' />
-        <h1>Grace Wambui Mwangi</h1>
-        <h2>Software engineer and Data scientist</h2>
-        <div className="card-links">
-            <a href="">Github</a>
-            <a href="">LinkedIn</a>
-            <a href="mailto:professorgee227@gmail.com">professorgee227@gmail.com</a>
-            <a href="">Personal website</a>
+    <div className='max-w-sm mx-auto mt-10 p-6 bg-white rounded-xl space-y-4  '>
+        <img src={myImage} alt="Picture of Grace" className='mx-auto w-32 rounded-full object object-cover'/>
+        <h1 className='text-2xl font-bold text-teal-800'>Grace Wambui Mwangi</h1>
+        <h2 className='text-lg text-teal-500'>Software engineer and Data scientist</h2>
+        <div className="flex justify-center space-x-4 pt-2">
+            <a href="" className='text-teal-600 hover:text-teal-300'>Github</a>
+            <a href="" className='text-teal-600 hover:text-teal-300'>LinkedIn</a>
+            <a href="mailto:professorgee227@gmail.com" className='text-teal-600 hover:text-teal-300'>Email</a>
+            <a href="" className='text-teal-600 hover:text-teal-300'>Website</a>
         </div>
     </div>
   )
